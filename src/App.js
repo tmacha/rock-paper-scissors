90032a8c24ac99cff78855ac0093bd13a286295c
Tobias Macha <tobias-macha@gmx.de>
2024-06-03 14:48:01 +0200

    setup basic game logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import { useState } from "react";
+import React from "react";
 
-function Player({ setChoice }) {
+function Player({ setChoice, setGame }) {
   return (
     <div className="player">
       <button
         onClick={() => {
+          setGame(true);
           setChoice(1);
         }}
       >
@@ -13,6 +15,7 @@ function Player({ setChoice }) {
       </button>
       <button
         onClick={() => {
+          setGame(true);
           setChoice(2);
         }}
       >
@@ -20,6 +23,7 @@ function Player({ setChoice }) {
       </button>
       <button
         onClick={() => {
+          setGame(true);
           setChoice(3);
         }}
       >
@@ -31,16 +35,40 @@ function Player({ setChoice }) {
 
 export default function Game() {
   const [player, setPlayerChoice] = useState(null);
+  const [text, setText] = useState("Waiting for your choice!");
+  const [game, setGame] = useState(false);
+
+  const choice = ["Rock", "Paper", "Scissor"];
+  const a = player;
+
+  if (game) {
+    setGame(false);
+    const b = Math.floor(Math.random() * 3) + 1;
+    if (a === b) {
+      setText(`${choice[a - 1]} vs ${choice[b - 1]} : Tie`);
+    } else if (a === 1 && b === 2) {
+      setText("Rock vs Paper : You lose");
+    } else if (a === 1 && b === 3) {
+      setText("Rock vs Scissors : You win");
+    } else if (a === 2 && b === 1) {
+      setText("Paper vs Rock : You win");
+    } else if (a === 2 && b === 3) {
+      setText("Paper vs Scissors : You lose");
+    } else if (a === 3 && b === 1) {
+      setText("Scissors vs Rock : You lose");
+    } else if (a === 3 && b === 2) {
+      setText("Scissors vs Paper : You win");
+    }
+  }
 
   return (
     <>
-      <Text />
-      <Player setChoice={setPlayerChoice} />
+      <Text text={text} />
+      <Player setChoice={setPlayerChoice} setGame={setGame} />
     </>
   );
 }
 
-function Text() {
-  const [text, setText] = useState("Waiting for your choice!");
+function Text({ text }) {
   return <div className="text">{text}</div>;
 }
